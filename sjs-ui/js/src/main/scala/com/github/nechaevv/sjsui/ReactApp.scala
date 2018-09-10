ea71af6ac65d0bcb0e5ef1486150df58cac05433
Vasiliy Nechaev <vn.glider@gmail.com>
2018-09-10 00:49:51 -0700

    factor out app pipeline

---
+++ sjs-ui/js/src/main/scala/com/github/nechaevv/sjsui/ReactApp.scala
@@ -0,0 +1,30 @@
+package com.github.nechaevv.sjsui
+
+import cats.effect._
+import cats.syntax.all._
+import com.github.nechaevv.sjsui.react.{ReactDOM, ReactRenderer}
+import fs2._
+import fs2.concurrent.Queue
+import org.scalajs.dom.Element
+
+object ReactApp {
+  def run[AppEvent, AppState, AppComponent <: platform.Component[AppState, AppEvent]]
+    (container: Element, appComponent: AppComponent, stateReducer: Reducer[AppEvent, AppState], initialState: AppState,
+     appStartEvent: AppEvent)(implicit concurrent: Concurrent[IO]): IO[Unit] = {
+    val stream = for {
+      eventStream ← Stream.eval(Queue.unbounded[IO, AppEvent])
+      _ ← Stream.eval(eventStream.enqueue1(appStartEvent))
+      eventDispatcher = new QueueEventDispatcher[AppEvent](eventStream)
+      events = eventStream.dequeue
+      state ← events.scan(initialState)((state, event) ⇒ {
+        val reducer: AppState ⇒ AppState = if (stateReducer.isDefinedAt(event)) stateReducer(event) else s ⇒ s
+        reducer(state)
+      })
+      _ ← Stream.eval(IO {
+        val reactComponent = appComponent(state, eventDispatcher)(ReactRenderer)
+        ReactDOM.render(reactComponent, container)
+      } )
+    } yield ()
+    stream.compile.drain
+  }
+}
--- src/main/scala/com/github/nechaevv/core/Events.scala
+++
@@ -1,7 +0,0 @@
-package com.github.nechaevv.core
-
-case class EventType(domName: String)
-
-object EventTypes {
-  val Click = EventType("click")
-}
