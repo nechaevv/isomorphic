159c504224fc60be48ebdd7bbc53ab848554e1c7
Vasiliy Nechaev <vn.glider@gmail.com>
2018-10-10 17:11:03 -0700

    Devtools Extension API

---
+++ core/js/src/main/scala/com/github/nechaevv/isomorphic/api/ReduxDevToolsExtension.scala
@@ -0,0 +1,28 @@
+package com.github.nechaevv.isomorphic.api
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+
+@js.native
+@JSGlobal
+class ReduxDevToolsExtension extends js.Object {
+  def connect(options: js.Any): ReduxDevToolsConnection = js.native
+  def disconnect(): Unit = js.native
+}
+
+@js.native
+@JSGlobal
+class ReduxDevToolsConnection extends js.Object {
+  def send(action: js.Any, state: js.Any): Unit = js.native
+  def subscribe(listener: js.Function1[js.Any, Unit]): Unit = js.native
+  def unsubscribe(): Unit = js.native
+  def init(state: js.Any): Unit = js.native
+  def error(message: String): Unit = js.native
+}
+
+object ReduxDevToolsExtension {
+  private val instance = js.Dynamic.global.window.__REDUX_DEVTOOLS_EXTENSION__
+  def isAvailable: Boolean = js.isUndefined(instance)
+  def apply(): ReduxDevToolsExtension = instance.asInstanceOf[ReduxDevToolsExtension]
+}
+
