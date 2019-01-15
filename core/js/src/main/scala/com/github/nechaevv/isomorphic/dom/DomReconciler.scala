de710b4fa009e6066e56cf46b9efebd543d6de0a
Vasiliy Nechaev <vn.glider@gmail.com>
2019-01-14 18:04:42 -0800

    ...

--- core/js/src/main/scala/com/github/nechaevv/isomorphic/dom/DomReconciler.scala
+++ core/js/src/main/scala/com/github/nechaevv/isomorphic/dom/DomReconciler.scala
@@ -4,7 +4,14 @@ import com.github.nechaevv.isomorphic.EventDispatcher
 import org.w3c.dom.html.HTMLElement
 
 object DomReconciler {
-  def apply(vdomNew: Node, vdomCurrent: Node, container: HTMLElement, eventDispatcher: EventDispatcher): Unit = {
-    ???
+  def apply(vdom: Seq[Node], context: DomReconciliationContext): DomReconciliationContext = {
+    reconcileNodeSeq(0, context.currentVDom, vdom, context.container, context.eventDispatcher)
+    context.copy(currentVDom = vdom)
+  }
+
+  private def reconcileNodeSeq(offset: Int, currentVDom: Seq[Node], vdom: Seq[Node], container: HTMLElement, eventDispatcher: EventDispatcher): Unit = {
+
   }
 }
+
+case class DomReconciliationContext(container: HTMLElement, eventDispatcher: EventDispatcher, currentVDom: Seq[Node])
