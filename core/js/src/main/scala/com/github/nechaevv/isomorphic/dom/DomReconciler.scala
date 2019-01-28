8ba858b45055100bff5d16406cd238cab29e9d5e
Vasiliy Nechaev <vn.glider@gmail.com>
2019-01-28 07:42:57 -0800

    ...

--- core/js/src/main/scala/com/github/nechaevv/isomorphic/dom/DomReconciler.scala
+++ core/js/src/main/scala/com/github/nechaevv/isomorphic/dom/DomReconciler.scala
@@ -9,7 +9,17 @@ import scala.scalajs.js
 object DomReconciler {
 
   private val componentCache = new WeakMap[ComponentNode[_], Node]()
-  private val childComponentDictionary = new WeakMap[Node, js.Dictionary[Node]]
+  private val nodeReprCache = new WeakMap[Node, NodeRepr]
+  private val childNodeCache = new WeakMap[NodeRepr, Map[String, NodeRepr]]
+
+  trait NodeRepr {
+    def key: String
+  }
+
+  case class TagRepr(name: String, attrs: Map[String, String], eventListeners: Set[NodeEventListener],
+                     children: Seq[NodeRepr], key: String) extends NodeRepr
+  case class FragmentRepr(children: Seq[NodeRepr], key: String) extends NodeRepr
+  case class TextRepr(text:String, key: String) extends NodeRepr
 
   def apply(vdom: Seq[Node], context: DomReconciliationContext): DomReconciliationContext = {
     reconcileNodeSeq(context.currentVDom, vdom, context.container, context.eventDispatcher)
@@ -21,8 +31,27 @@ object DomReconciler {
     var index = 0
     for (node ← vdom) {
       var key = node.key.getOrElse({ index += 1; index.toString })
+
     }
   }
+
+  /*
+  private def buildRepr(vdom: Seq[Node]): Seq[NodeRepr] = vdom map {
+    case TagNode(name, props, children, key) ⇒ TagRepr(name,
+      props.collect({
+        case NodeAttribute(attrName, value) ⇒ attrName → value
+      }).toMap,
+      props.collect({
+        case e: NodeEventListener ⇒ e
+      }).toSet,
+      buildRepr(children)
+    )
+    case TextNode(s) ⇒ TextRepr(s)
+    case f: FragmentNode ⇒ FragmentRepr(buildRepr(f.children))
+    case cn: ComponentNode[_] ⇒
+      val cached
+  }
+  */
 
 }
 
