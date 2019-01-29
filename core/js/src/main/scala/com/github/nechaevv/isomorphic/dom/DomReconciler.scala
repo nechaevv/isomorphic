80dee93cad74dc580f31d9a76c412090e2e12578
Vasiliy Nechaev <vn.glider@gmail.com>
2019-01-28 17:19:15 -0800

    text node reconciliation

--- core/js/src/main/scala/com/github/nechaevv/isomorphic/dom/DomReconciler.scala
+++ core/js/src/main/scala/com/github/nechaevv/isomorphic/dom/DomReconciler.scala
@@ -2,15 +2,16 @@ package com.github.nechaevv.isomorphic.dom
 
 import com.github.nechaevv.isomorphic.EventDispatcher
 import com.github.nechaevv.isomorphic.api.WeakMap
-import org.w3c.dom.html.HTMLElement
+import org.scalajs.dom.document
+import org.scalajs.dom.raw.HTMLElement
 
 import scala.scalajs.js
 
 object DomReconciler {
 
-  private val componentCache = new WeakMap[ComponentNode[_], Node]()
+  private val componentCache = new WeakMap[ComponentNode[_], Node]
   private val nodeReprCache = new WeakMap[Node, NodeRepr]
-  private val childNodeCache = new WeakMap[NodeRepr, Map[String, NodeRepr]]
+  private val elementMapping = new WeakMap[NodeRepr, org.scalajs.dom.Node]
 
   trait NodeRepr {
     def key: String
@@ -28,10 +29,39 @@ object DomReconciler {
 
   private def reconcileNodeSeq(currentVDom: Seq[Node], vdom: Seq[Node], container: HTMLElement,
                                eventDispatcher: EventDispatcher): Unit = {
-    var index = 0
-    for (node ← vdom) {
-      var key = node.key.getOrElse({ index += 1; index.toString })
+    var currentVdomRepr = currentVDom.map(nodeReprCache.get(_).toOption).zipWithIndex
+    var recycledElements = List.empty[org.w3c.dom.Node]
+    var auxId = 0
+    for ((node, nodeIndex) ← vdom.zipWithIndex) {
+      var nodeRepr = nodeReprCache
+      var key = node.key.getOrElse({ auxId += 1; "$" + auxId.toString})
+      var matchingNodeOpt = currentVdomRepr.find(el ⇒ el._1.exists(_.key == key)).map({
+        case (Some(el), elIndex) ⇒ el → elIndex
+      })
+      var replaceNode = matchingNodeOpt.isEmpty || matchingNodeOpt.exists(_._2 != nodeIndex)
 
+      node match {
+        case TagNode(name, props, children, _) ⇒
+
+        case FragmentNode(children, _) ⇒
+
+        case TextNode(text) ⇒
+          val (node, nodeRepr) = (for {
+            (textNodeRepr: TextRepr, textNodeIndex) ← matchingNodeOpt
+            domNode ← elementMapping.get(textNodeRepr).toOption if domNode.isInstanceOf[org.w3c.dom.Text]
+          } yield {
+            if (textNodeRepr.text != text) {
+              domNode.asInstanceOf[org.w3c.dom.Text].replaceWholeText(text)
+              val repr = TextRepr(text, key)
+              elementMapping.set(repr, domNode)
+              (domNode, repr)
+            } else (domNode, textNodeRepr)
+          }).getOrElse((document.createTextNode(text), TextRepr(text, key)))
+          elementMapping.set(nodeRepr, node)
+          if (replaceNode) {
+            container.appendChild(node)
+          }
+      }
     }
   }
 
